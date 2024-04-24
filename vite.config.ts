6e8ae512fa68e80b4fa89405df8510b82498f461
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-04-24 20:05:50 +0300

    feat: add assets alias to config files, edit import declaration

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
       "@atoms": path.resolve(__dirname, "./src/components/atoms"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
     },
   },
   plugins: [react()],
