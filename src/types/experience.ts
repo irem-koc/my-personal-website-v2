7c5102e4d1bbbc86a2b795d3678421cff0d870cb
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-02 16:07:01 +0300

    feat: add type check

---
+++ src/types/experience.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+    id: number;
+    title: string;
+    company: string;
+    location: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+}
