a7fb1e81e0d1a6c96696f9054b2bdaeb762a889b
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-02 13:13:55 +0300

    feat: edit about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -10,7 +10,7 @@ const About = (props: Props) => {
   return (
     <div className="lg:p-28 p-32 bg-about-bg w-full h-screen flex flex-col justify-around">
       <div className="row-between">
-        <div className="col-start w-1/3">
+        <div className="hidden xl:flex col-start w-1/3">
           <Text text="- NICE TO MEET YOU!" style={["about-top-text"]} />
           <Title text={"Irem Koc"} style={["about-title"]} />
           <Text
@@ -25,7 +25,7 @@ const About = (props: Props) => {
             to={"/portfolio"}
           />
         </div>
-        <div className="w-5/6">
+        <div className="xl:w-5/6">
           <div>
             <Text
               text={"Hello there! My name is "}
@@ -38,26 +38,6 @@ const About = (props: Props) => {
               }
               style={["about-info-text"]}
             />
-            <br />
-            <br />
-            {/* <Text
-              text={
-                "With a solid foundation in technologies like React, React Native (both CLI and Expo), TypeScript, and Redux Toolkit, I am well-equipped to deliver robust and user-friendly applications. "
-              }
-              style={[]}
-            />
-            <Text
-              text={
-                "In addition to my frontend expertise, I also have experience with backend development using Spring Boot, which allows me to understand and contribute to different layers of a software project. "
-              }
-              style={[]}
-            />
-            <Text
-              text={
-                "I am passionate about every step of the software development process, from initial discussion and collaboration to the final stages of deployment. "
-              }
-              style={[]}
-            /> */}
             <Text
               text={
                 "With over 2 years of experience in the field, I have honed the skills necessary to bring your project to success. I look forward to the opportunity to leverage my skills and enthusiasm to contribute to your team."
@@ -65,7 +45,7 @@ const About = (props: Props) => {
               style={["about-info-text"]}
             />
           </div>
-          <div className="row-between">
+          <div className="hidden my-5 xl:flex row-between">
             <div className="col-center">
               <Text text={"AGE"} style={["underline"]} />
               <Text text={"22"} style={["font-bold text-dark-blue"]} />
@@ -84,10 +64,10 @@ const About = (props: Props) => {
                 style={["font-bold text-dark-blue"]}
               />
             </div>
-            <div className="col-start">
+            <div className="col-center">
               <Text text={"PHONE"} style={["underline"]} />
               <Text
-                text={"+90 5539753691"}
+                text={"+90 553 975 3691"}
                 style={["font-bold text-dark-blue"]}
               />
             </div>
@@ -97,7 +77,7 @@ const About = (props: Props) => {
       <div className="row-between w-full gap-5">
         <InfoCard
           style={["info-card bg-[#D3F4EC]"]}
-          top={"3.18"}
+          top={"3.19"}
           bottom={"GPA"}
         />
         <InfoCard
