6d660977e7ea722b7e18753d582fae610609458e
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-05 22:39:39 +0300

    feat: add portfolio card component

--- src/components/molecules/PortfolioCard/PortfolioCard.tsx
+++ src/components/molecules/PortfolioCard/PortfolioCard.tsx
@@ -1,18 +1,30 @@
+import Link from "@components/atoms/Link/Link";
 import { FaArrowRight } from "react-icons/fa";
 
 type Props = {};
 
 const PortfolioCard = ({ project }: Props) => {
   return (
-    <div>
-      <div></div>
+    <div className="portfolio-card">
       <div>
-        <p>
-          Go to Github repository
-          <span>
-            <FaArrowRight fill="black" />
-          </span>
+        <img
+          className="w-full object-cover h-48"
+          src={project.thumbnail}
+          alt={project.title}
+        />
+        <p className="font-semibold italic text-black text-base">
+          {project.title}
         </p>
+      </div>
+      <div className="flex gap-2 items-center ">
+        <Link
+          text={"Go to Github repository"}
+          href={project.githubUrl}
+          style={["italic home-contact font-[13px] hover:translate-x-0"]}
+        />
+        <span>
+          <FaArrowRight fill="black" />
+        </span>
       </div>
     </div>
   );
