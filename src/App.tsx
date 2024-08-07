8a086e10193fb7641d3fa6c280cd257852aa1c0a
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-07 22:34:12 +0300

    fix: remove context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import LeftMenu from "@components/organisms/LeftMenu/LeftMenu";
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import ContextProvider from "./context/Context";
 
 function App() {
   return (
-    <ContextProvider>
-      <div className="flex">
-        <LeftMenu />
-        <Outlet />
-      </div>
-    </ContextProvider>
+    <div className="flex">
+      <LeftMenu />
+      <Outlet />
+    </div>
   );
 }
 
