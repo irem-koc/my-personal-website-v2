a0fd2864e155fb0cb7594be43dab3768aa382f84
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-08-14 20:07:46 +0300

    feat: add about page responsiveness

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,9 +5,9 @@ import InfoCard from "@components/molecules/InfoCard/InfoCard";
 
 const About = () => {
   return (
-    <div className="lg:p-28 p-32 bg-about-bg w-full h-screen flex flex-col justify-around">
-      <div className="row-between">
-        <div className="hidden xl:flex col-start w-1/3">
+    <div className="lg:p-28 md:p-16 p-8 bg-about-bg w-full min-h-screen flex flex-col justify-around">
+      <div className="flex flex-col xl:flex-row justify-between items-start">
+        <div className="xl:flex hidden flex-col w-1/3">
           <Text text="- NICE TO MEET YOU!" style={["about-top-text"]} />
           <Title text={"Irem Koc"} style={["about-title"]} />
           <Text
@@ -22,7 +22,7 @@ const About = () => {
             to={"/portfolio"}
           />
         </div>
-        <div className="xl:w-5/6">
+        <div className="w-full xl:w-5/6 mt-8 xl:mt-0">
           <div>
             <Text
               text={"Hello there! My name is "}
@@ -31,7 +31,7 @@ const About = () => {
             <Text text={"Irem Koc"} style={["text-orange about-info-text"]} />
             <Text
               text={
-                ". As a software engineer with a focus on frontend development, I bring a strong dedication and a wealth of experience to my work. "
+                ". As a software engineer with a focus on frontend development, I bring a strong dedication and a wealth of experience to my work."
               }
               style={["about-info-text"]}
             />
@@ -42,26 +42,26 @@ const About = () => {
               style={["about-info-text"]}
             />
           </div>
-          <div className="hidden my-5 xl:flex row-between">
-            <div className="col-center">
+          <div className="flex flex-wrap justify-between gap-5 my-5 xl:flex-row">
+            <div className="col-center w-full sm:w-auto">
               <Text text={"AGE"} style={["underline"]} />
               <Text text={"22"} style={["font-bold text-dark-blue"]} />
             </div>
-            <div className="col-center">
+            <div className="col-center w-full sm:w-auto">
               <Text text={"BORN IN"} style={["underline"]} />
               <Text
                 text={"Istanbul, Turkey"}
                 style={["font-bold text-dark-blue"]}
               />
             </div>
-            <div className="col-center">
+            <div className="col-center w-full sm:w-auto">
               <Text text={"MAIL"} style={["underline"]} />
               <Text
                 text={"kociremx@gmail.com"}
                 style={["font-bold text-dark-blue"]}
               />
             </div>
-            <div className="col-center">
+            <div className="col-center w-full sm:w-auto">
               <Text text={"PHONE"} style={["underline"]} />
               <Text
                 text={"+90 553 975 3691"}
@@ -71,19 +71,19 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className="row-between w-full gap-5">
+      <div className="flex flex-wrap justify-between gap-5 w-full mt-8">
         <InfoCard
-          style={["info-card bg-[#D3F4EC]"]}
+          style={["info-card bg-[#D3F4EC] w-full sm:w-auto"]}
           top={"3.19"}
           bottom={"GPA"}
         />
         <InfoCard
-          style={["info-card bg-[#FCE8D4]"]}
+          style={["info-card bg-[#FCE8D4] w-full sm:w-auto"]}
           top={"30+"}
           bottom={"PROJECTS COMPLETED"}
         />
         <InfoCard
-          style={["info-card bg-[#E3F9E0]"]}
+          style={["info-card bg-[#E3F9E0] w-full sm:w-auto"]}
           top={"2+"}
           bottom={"YEARS OF EXPERIENCE"}
         />
